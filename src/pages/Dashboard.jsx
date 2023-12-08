f225badf53afdb7ec9db6166551586700830d839
zaryabdogar <zaryabdogar23@gmail.com>
2023-12-08 20:27:34 +0500

    commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,12 @@
 const Dashboard = () => {
 	return (
-		<section className=" p-4 bg-[#000000] py-12">
+		<section className=" p-4 bg-[#000000] sm:py-0 py-12">
 			<div className="flex flex-col-reverse lg:flex-row-reverse lg:justify-between    bg-[#106466] ">
 				{/* Image Section */}
 				<div className="lg:w-[45%] py-8 flex justify-center bg-[#D9DEE4]   items-center">
 					<img
 						src="/robo.png"
-						className="xl:w-[300px] xl:h-[420px] lg:w-[200px] lg:h-[322px] md:w-[200] md:h-[322px] w-[200px] h-[322px]"
+						className="xl:w-[300px] xl:h-[420px] lg:w-[200px] lg:h-[322px] sm:w-[200px] sm:h-[322px] w-[100px] h-[202px]"
 						alt="robo"
 					/>
 				</div>
